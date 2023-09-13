62ca13e4116ea8114afd557ec3786381e476e0a1
inari-x <katrinmalfent@gmail.com>
2023-09-13 07:13:18 -0700

    call button, press translate, responds with translation: complete

---
+++ README.md
@@ -0,0 +1,15 @@
+Setup
+
+clone the repository
+Windows: setup a subsystem for linux like WSL and clone the repository in there. 
+
+run sudo apt-get install python3-virtualenv python3-pip curl jq
+naviagate in dbot folder and setup virtual environment
+(virtualenv bot)
+activate the virtual environment (source bot/bin/activate)
+
+run: pip install discord.py && pip install python-dotenv
+
+to run the bot: 
+python3 bot.py
+
--- dbot/bot.py
+++ dbot/bot.py
@@ -2,11 +2,17 @@ import os
 import asyncio
 import discord
 from discord.ext import commands 
+import requests
+
+# fix later - https://pypi.org/project/hotreload/
+# import time
+# import logging
+# from hotreload import Loader
 
 import context_handler
-import datetime
-
+from request_queue import init_request_queue, llm_complete_request
 from dotenv import load_dotenv
+
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
@@ -15,144 +21,137 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix=".", intents=intents)
 
+llama2_url = "http://127.0.0.1:8000/v1/completions/"
+
+def translate_text(prompt, source_language, target_language):
+    payload = {
+        "prompt": f"\n\n### Translate the following from {source_language} to {target_language}:\n{prompt}\n\n### Response:\n",
+            "stop": [
+                "\n",
+                "###"
+            ]
+    }
+
+    try:
+        # Make a POST request to your custom translation endpoint
+        response = requests.post(llama2_url, json=payload)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse and return the translated text from the response
+            # {
+            #     "id": "cmpl-903a5976-f714-4ff1-93fe-16562853634a",
+            #     "object": "text_completion",
+            #     "created": 1694613063,
+            #     "model": "../wizardlm-13b-v1.2.Q5_K_M.gguf",
+            #     "choices": [
+            #         {
+            #             "text": "你好，您如何？",
+            #             "index": 0,
+            #             "logprobs": null,
+            #             "finish_reason": "stop"
+            #         }
+            #     ],
+            #     "usage": {
+            #         "prompt_tokens": 29,
+            #         "completion_tokens": 9,
+            #         "total_tokens": 38
+            #     }
+            # }
+            # get choices -> text
+            translated_text = response.json().get("choices")[0].get("text")
+            # translated_text = response.json().get("translated_text")
+            return translated_text
+        else:
+            # Handle errors or provide an appropriate response
+            return "Sorry, there was an error with the translation."
+
+    except Exception as e:
+        # Handle exceptions or errors
+        return str(e)
+
 class HelpButtons(discord.ui.View):
     def __init__(self):
         super().__init__()
 
-        # Create buttons with labels
-        button1 = discord.ui.Button(
-            style=discord.ButtonStyle.gray,
-            label="Button 1",
-            custom_id="button1"
-        )
-        button2 = discord.ui.Button(
-            style=discord.ButtonStyle.gray,
-            label="Button 2",
-            custom_id="button2"
-        )
-        button3 = discord.ui.Button(
-            style=discord.ButtonStyle.gray,
-            label="Button 3",
-            custom_id="button3"
-        )
+    @discord.ui.button(label="Translate", style=discord.ButtonStyle.green)
+    async def button1(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # Prompt the user for the text to translate
+        await interaction.response.send_message(content="Please provide the text to translate formatted as '<source language> <target language> <message>'\nEx: german english how are you?")
 
-        # Add buttons to the view
-        self.add_item(button1)
-        self.add_item(button2)
-        self.add_item(button3)
+        # Wait for a response from the user
+        try:
+            response = await client.wait_for(
+                "message",
+                check=lambda m: m.author == interaction.user and m.channel == interaction.channel,
+                timeout=60.0  # Adjust the timeout as needed
+            )
+        except asyncio.TimeoutError:
+            await interaction.followup.send("Translation request timed out.")
+            return
 
-    @discord.ui.button(label="Button 1")
-    async def button1(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.response.send_message(content="This is an edited response for Button 1!")
+        text_to_translate = response.content
+        # TODO - log here the message
+        print(f"Received message: {text_to_translate} from {response.author}")
 
-    @discord.ui.button(label="Button 2")
-    async def button2(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.response.send_message(content="This is an edited response for Button 2!")
+        # example translation request is as formatted:
+        # english german "hello, how are you?"
+        # break up text_to_translate by first word, second word, rest of message into 3 variables
+        source_language = text_to_translate.split(" ")[0]
+        target_language = text_to_translate.split(" ")[1]
+        text_to_translate = " ".join(text_to_translate.split(" ")[2:])
 
-    @discord.ui.button(label="Button 3")
-    async def button3(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.response.send_message(content="This is an edited response for Button 3!")
+        # Call the translate_text function to get the translation
+        translated_text = translate_text(text_to_translate, source_language, target_language)
 
+        # Send the translated text as a response
+        await interaction.followup.send(f"Translation:\n {translated_text}", view=HelpButtons())
+
+
+# @client.event
+# async def on_ready():
+#     await init_request_queue()
+#     print(f'logged in as {client.user}')
+
+# @client.event
+# async def on_message(message):
+#     is_dm = isinstance(message.channel, discord.channel.DMChannel)
+#     at_mention = f'<@{client.user.id}>'
+#     context_id = str(message.channel)
+#     if is_dm:
+#         context_id = str(message.author.id)
+#     # we do not want the bot to reply to itself
+#     if message.author.id == client.user.id:
+#         return
+
+#     # If message is DM or @sandbox
+#     if is_dm or message.content.startswith(at_mention):
+#         # console log here
+#         print(f"Received message: {message.content} from {message.author}")
+#         stripped_message = str(message.content).replace(at_mention, '').strip()
+#         if stripped_message.startswith("!reset"):
+#             reset_result = context_handler.reset_context(context_id)
+#             await message.reply(reset_result, mention_author=True)
+#             return
+#         if stripped_message.startswith("!complete"):
+#             complete_response = await llm_complete_request(stripped_message, context_id, complete_only=True)
+#             await message.reply(complete_response, mention_author=True)
+#             return
+#         complete_response = await llm_complete_request(stripped_message, context_id, complete_only=False)
+#         await message.reply(complete_response, mention_author=True)
+            
+        
 @client.command()
 async def button(ctx):
-    await ctx.send("This message has buttons!", view=HelpButtons())
+    message = await ctx.send("Welcome to the bot!", view=HelpButtons())
+    ctx.view.message = message
 
-client.run(TOKEN)
-
-
-
-
-# class MyView(discord.ui.View):
-#     async def on_timeout(self) -> None:
-#         # Step 2
-#         for item in self.children:
-#             item.disabled = True
-
-#         # Step 3
-#         await self.message.edit(view=self)
-
-#     @discord.ui.button(label='Example')
-#     async def example_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-#         await interaction.response.send_message('Hello!', ephemeral=True)
-
-# @bot.command()
-# async def timeout_example(ctx):
-#     """An example to showcase disabling buttons on timing out"""
-#     view = MyView()
-#     # Step 1
-#     view.message = await ctx.send('Press me!', view=view)
-
-# class MyClient(discord.Client):
-#     async def on_ready(self):
-#         await init_request_queue()
-#         print('Current time: ', datetime.datetime.now())
-#         print(f'User: {self.user} (ID: {self.user.id})')
-#         print('Warming up...')
-
-#     async def on_message(self, message):
-#         is_dm = isinstance(message.channel, discord.channel.DMChannel)
-#         at_mention = f'<@{self.user.id}>'
-#         context_id = str(message.channel)
-#         if is_dm:
-#             context_id = str(message.author.id)
-#         # we do not want the bot to reply to itself
-#         if message.author.id == self.user.id:
-#             return
-
-#         # If message is DM or @sandbox
-#         if is_dm or message.content.startswith(at_mention):
-#             # console log here
-#             print(f"Received message: {message.content} from {message.author}")
-#             stripped_message = str(message.content).replace(at_mention, '').strip()
-#             if stripped_message.startswith("!reset"):
-#                 reset_result = context_handler.reset_context(context_id)
-#                 await message.reply(reset_result, mention_author=True)
-#                 return
-#             if stripped_message.startswith("!complete"):
-#                 complete_response = await llm_complete_request(stripped_message, context_id, complete_only=True)
-#                 await message.reply(complete_response, mention_author=True)
-#                 return
-#             complete_response = await llm_complete_request(stripped_message, context_id, complete_only=False)
-#             await message.reply(complete_response, mention_author=True)
-            
-#         # add a function for !help with an embedded message
-#         if message.content.startswith("!help"):
-#             embed = discord.Embed(title="Help", description="List of my commands are:", color=0xeee657)
-
-#             embed.add_field(name="!help", value="Gives this message.", inline=False)
-#             embed.add_field(name="!complete", value="Completes the given sentence.\n!complete <to be determined>", inline=False)
-#             embed.add_field(name="!translate", value="Command parameters are source language, origin language and message.\n!translate german english bitte", inline=False)
-#             embed.add_field(name="!enhance", value="Command parameters are method and message.\n!enhance \"make simpler\" We are not particulary engaged at this time", inline=False)
-            
-#             # add a discord.Button to the embed 
-#             # button_1 = discord.ui.Button(label="Translate", url="https://google.com")
-#             # button_2 = discord.ui.Button(label="Complete", url="https://google.com")
-#             # button_3 = discord.ui.Button(label="Enhance", url="https://google.com")
-
-
-
-#             # add button1, button2 and button3 to the embed as components
-#             # action_row = discord.ActionRow(button_1, button_2, button_3)
-#             #ActionRow takes children as a list of components and type 
-
-#             #                              children                         type
-#             # discord.ActionRow( [button_1, button_2, button_3], type=discord.ComponentType.action_row)
-
-#             # add action_row to the embed
-
-#             embed.set_footer(text="Click the button to go to the website")
-#             embed.set_thumbnail(url="https://i.imgur.com/axLm3p6.png")
-#             embed.set_author(name="LLM Bot", url="", icon_url="https://i.imgur.com/axLm3p6.png")
-#             embed.add_field(name="Website (google.com)", value="https://google.com", inline=False)
-            
-
-#             # await message.channel.send(embed=embed, components=[action_row])
-#             await message.channel.send(embed=embed)
-
-
-# # # add ! prefix to bot commands
-
-
-# client = MyClient(intents=intents)
-client.run(TOKEN)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+ 
+    await client.process_commands(message)
+    
+client.run(TOKEN) 
 
