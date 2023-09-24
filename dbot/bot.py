fd742db10a5faeacad19b26a9138f39de884e54c
inari-x <katrinmalfent@gmail.com>
2023-09-23 23:51:20 -0700

    FIFO queue functions

--- dbot/bot.py
+++ dbot/bot.py
@@ -11,6 +11,7 @@ REQUEST_QUEUE_TRANSLATION = asyncio.Queue()
 RESPONSE_QUEUE_TRANSLATION = asyncio.Queue()
 REQUEST_QUEUE_ENHANCEMENT = asyncio.Queue()
 RESPONSE_QUEUE_ENHANCEMENT = asyncio.Queue()
+REQUEST_TYPE = ''
 
 
 # fix later - https://pypi.org/project/hotreload/
@@ -42,16 +43,22 @@ async def translate_text(prompt, source_language, target_language):
     }
 
     try:
-        await REQUEST_QUEUE_TRANSLATION.put((prompt, source_language, target_language))
-        print(f"Translation request: {prompt} from {source_language} to {target_language}")
+        REQUEST_TYPE = "translation"
+        # await REQUEST_QUEUE_TRANSLATION.put((prompt, source_language, target_language))
+        # print(f"Translation request: {prompt} from {source_language} to {target_language}")
         response = requests.post(llama2_url, json=payload)
-        print(f"Translation size of queue: {REQUEST_QUEUE_TRANSLATION.qsize()}")
+        await REQUEST_QUEUE_TRANSLATION.put((prompt, source_language, target_language))
+        print(f"Size of REQqueue: {REQUEST_QUEUE_TRANSLATION.qsize()}")
+        # print(f"Translation size of queue: {REQUEST_QUEUE_TRANSLATION.qsize()}")
 
         if response.status_code == 200:
             translated_text = response.json().get("choices")[0].get("text")
-            RESPONSE_QUEUE_TRANSLATION = response.json()
-            print(f"Translation response: {RESPONSE_QUEUE_TRANSLATION}")
-            return translated_text
+            REQUEST_QUEUE_TRANSLATION.task_done()
+            await RESPONSE_QUEUE_TRANSLATION.put(translated_text)
+            print(f"Size of RESqueue: {RESPONSE_QUEUE_TRANSLATION.qsize()}")
+            # RESPONSE_QUEUE_TRANSLATION = response.json()
+            # print(f"Translation response: {RESPONSE_QUEUE_TRANSLATION}")
+            return translated_text, REQUEST_TYPE
         else:
             return "Sorry, there was an error with the translation."
 
@@ -69,15 +76,18 @@ async def complete_text(prompt):
     }
 
     try:
-        REQUEST_QUEUE_COMPLETION.put(prompt)
-        print(f"Completion request: {prompt}")
+        REQUEST_TYPE = "completion"
         response = requests.post(llama2_url, json=payload)
+        await REQUEST_QUEUE_COMPLETION.put(prompt)
+        print(f"Size of REQqueue: {REQUEST_QUEUE_COMPLETION.qsize()}")
 
         if response.status_code == 200:
             completed_text = response.json().get("choices")[0].get("text")
-            RESPONSE_QUEUE_COMPLETION = response.json()
-            print(f"Completion response: {RESPONSE_QUEUE_COMPLETION}")
-            return completed_text
+            REQUEST_QUEUE_COMPLETION.task_done()
+            await RESPONSE_QUEUE_COMPLETION.put(response.json())
+            print(f"Size of RESqueue: {RESPONSE_QUEUE_COMPLETION.qsize()}")
+
+            return completed_text, REQUEST_TYPE
         else:
             return "Sorry, there was an error with the completion."
         
@@ -86,7 +96,7 @@ async def complete_text(prompt):
 
     
 
-def enhance_text(prompt):
+async def enhance_text(prompt):
     payload = {
         "prompt": f"\n\n### Enhance the following:\n{prompt}\n\n### Response:\n",
             "stop": [
@@ -97,29 +107,78 @@ def enhance_text(prompt):
     }
 
     try:
-        REQUEST_QUEUE_ENHANCEMENT.put(prompt)
-        print(f"Enhancement request: {prompt}")
+        REQUEST_TYPE = "enhancement"
         response = requests.post(llama2_url, json=payload)
+        await REQUEST_QUEUE_ENHANCEMENT.put(prompt)
+        print(f"Size of REQqueue: {REQUEST_QUEUE_ENHANCEMENT.qsize()}")
+
 
 
         if response.status_code == 200:
             enhanced_text = response.json().get("choices")[0].get("text")
-            RESPONSE_QUEUE_ENHANCEMENT = response.json()
-            print(f"Enhancement response: {RESPONSE_QUEUE_ENHANCEMENT}")
-            return enhanced_text
+            REQUEST_QUEUE_ENHANCEMENT.task_done()
+            await RESPONSE_QUEUE_ENHANCEMENT.put(response.json())
+            print(f"Size of RESqueue: {RESPONSE_QUEUE_ENHANCEMENT.qsize()}")
+            return enhanced_text, REQUEST_TYPE
         else:
             return "Sorry, there was an error with the enhancement."
         
     except Exception as e:
         return str(e)
     
-    
+async def queue_worker(queue, response_queue, processing_function, REQUEST_TYPE):
+    while True:
+        print(f"Waiting for {REQUEST_TYPE} request...")
+        if(REQUEST_TYPE == "completion"): 
+            queue = REQUEST_QUEUE_COMPLETION
+            item = await queue.get()
+            if item is None:
+                break  # Exit the loop when the queue is empty
+            print(f"Processing {REQUEST_TYPE} request:", item)
+            print(f"Size of {REQUEST_TYPE} REQqueue: {queue.qsize()}")
+
+        elif(REQUEST_TYPE == "translation"):
+            queue = REQUEST_QUEUE_TRANSLATION
+            item = await queue.get()
+            if item is None:
+                break
+            print(f"Processing {REQUEST_TYPE} request:", item)
+            print(f"Size of {REQUEST_TYPE} REQqueue: {queue.qsize()}")
+
+        elif(REQUEST_TYPE == "enhancement"):
+            queue = REQUEST_QUEUE_ENHANCEMENT
+            item = await queue.get()
+            if item is None:
+                break
+            print(f"Processing {REQUEST_TYPE} REQrequest:", item)
+        
+        # try:
+        #     result = await processing_function(*item)  # Unpack and pass arguments
+        # except Exception as e:
+        #     print(f"Error processing {REQUEST_TYPE} request: {str(e)}")
+        #     result = str(e)  # Assign an error message in case of an exception
+        # queue.task_done()
+        # print(f"Size of {REQUEST_TYPE} queue: {queue.qsize()}")
+        # print(f"Finished processing {REQUEST_TYPE} request:", item)
+        # await response_queue.put(result)
+
+async def init_request_queues():
+    await asyncio.gather(
+        queue_worker(REQUEST_QUEUE_COMPLETION, RESPONSE_QUEUE_COMPLETION, complete_text, "completion"),
+        queue_worker(REQUEST_QUEUE_TRANSLATION, RESPONSE_QUEUE_TRANSLATION, translate_text, "translation"),
+        queue_worker(REQUEST_QUEUE_ENHANCEMENT, RESPONSE_QUEUE_ENHANCEMENT, enhance_text, "enhancement")
+    )
+
+
 @client.event
 async def on_ready():
     print("Bot is ready")
     print("Completion:", REQUEST_QUEUE_COMPLETION.qsize())
     print("Translation:", REQUEST_QUEUE_TRANSLATION.qsize())
     print("Enhancement:", REQUEST_QUEUE_ENHANCEMENT.qsize())
+
+    await init_request_queues()
+
 
 class HelpButtons(discord.ui.View):
     def __init__(self):
@@ -148,6 +207,10 @@ class HelpButtons(discord.ui.View):
         # Call the translate_text function to get the translation
         translated_text = await translate_text(text_to_translate, source_language, target_language)
 
+        await RESPONSE_QUEUE_TRANSLATION.get()
+        RESPONSE_QUEUE_TRANSLATION.task_done()
+        print(f"Size of RESqueue: {RESPONSE_QUEUE_TRANSLATION.qsize()}")
+
         # Send the translated text as a response
         await interaction.followup.send(f"Translation:\n {translated_text}", view=HelpButtons())
 
@@ -171,6 +234,11 @@ class HelpButtons(discord.ui.View):
         # Call the complete_text function to get the completion
         completed_text = await complete_text(text_to_complete)
 
+        await RESPONSE_QUEUE_COMPLETION.get()
+        RESPONSE_QUEUE_COMPLETION.task_done()
+        print(f"Size of RESqueue: {RESPONSE_QUEUE_COMPLETION.qsize()}")
+
+
         # Send the completed text as a response
         await interaction.followup.send(f"Completion:\n {completed_text}", view=HelpButtons())
 
@@ -193,6 +261,10 @@ class HelpButtons(discord.ui.View):
 
         # Call the enhance_text function to get the enhancement
         enhanced_text = await enhance_text(text_to_enhance)
+
+        await RESPONSE_QUEUE_ENHANCEMENT.get()
+        RESPONSE_QUEUE_ENHANCEMENT.task_done()
+        print(f"Size of RESqueue: {RESPONSE_QUEUE_ENHANCEMENT.qsize()}")
 
         # Send the enhanced text as a response
         await interaction.followup.send(f"Enhancement:\n {enhanced_text}", view=HelpButtons())
@@ -221,24 +293,24 @@ async def on_message(message):
         await message.reply(reset_result, mention_author=True)
         return
     
-    if discord.ui.Button.label == "Translate":
-        print("Translate button pressed")
-        # Extract and enqueue translation request
-        text_to_translate = message.content[len("translate"):].strip()
-        await REQUEST_QUEUE_TRANSLATION.put((text_to_translate, context_id))
-        return
+    # if discord.ui.Button.label == "Translate":
+    #     print("Translate button pressed")
+    #     # Extract and enqueue translation request
+    #     text_to_translate = message.content[len("translate"):].strip()
+    #     await REQUEST_QUEUE_TRANSLATION.put((text_to_translate, context_id))
+    #     return
 
-    if discord.ui.Button.label == "Complete":
-        # Extract and enqueue completion request
-        text_to_complete = message.content[len("complete"):].strip()
-        await REQUEST_QUEUE_COMPLETION.put((text_to_complete, context_id))
-        return
+    # if discord.ui.Button.label == "Complete":
+    #     # Extract and enqueue completion request
+    #     text_to_complete = message.content[len("complete"):].strip()
+    #     await REQUEST_QUEUE_COMPLETION.put((text_to_complete, context_id))
+    #     return
 
-    if discord.ui.Button.label == "Enhance":
-        # Extract and enqueue enhancement request
-        text_to_enhance = message.content[len("enhance"):].strip()
-        await REQUEST_QUEUE_ENHANCEMENT.put((text_to_enhance, context_id))
-        return
+    # if discord.ui.Button.label == "Enhance":
+    #     # Extract and enqueue enhancement request
+    #     text_to_enhance = message.content[len("enhance"):].strip()
+    #     await REQUEST_QUEUE_ENHANCEMENT.put((text_to_enhance, context_id))
+    #     return
     
     await client.process_commands(message)
     
